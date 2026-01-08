32feb04f5a766397fbec064b4aab7718d795677d
Valentin Soare <valentin.soare@indaco.ro>
2026-01-08 02:25:54 +0200

    Initial commit

---
+++ src/main/kotlin/io/valentinsoare/noir/configuration/CoroutineConfiguration.kt
@@ -0,0 +1,25 @@
+package io.valentinsoare.noir.configuration
+
+import io.valentinsoare.noir.utils.IOScope
+import io.valentinsoare.noir.utils.ProcessingScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CoroutineConfiguration (
+    private val exceptionHandler: CoroutineExceptionHandler
+) {
+
+    @Bean
+    @ProcessingScope
+    fun processingScope(): CoroutineScope =
+        CoroutineScope(Dispatchers.Default + exceptionHandler)
+
+    @Bean
+    @IOScope
+    fun ioScope(): CoroutineScope =
+        CoroutineScope(Dispatchers.IO + exceptionHandler)
+}
